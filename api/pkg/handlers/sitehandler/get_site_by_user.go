83fc6f7ad319c2355c3ea12973ce9882026e15a3
FourLineCode <akmal3535.ah@gmail.com>
2021-09-24 10:24:17 +0600

    sitehandler init

---
+++ api/pkg/handlers/sitehandler/get_site_by_user.go
@@ -0,0 +1,21 @@
+package sitehandler
+
+import (
+	"github.com/FourLineCode/url-shortener/api/pkg/models"
+	"github.com/FourLineCode/url-shortener/api/pkg/utils"
+	"github.com/gofiber/fiber/v2"
+)
+
+func (h *SiteHandler) getSiteByUser(c *fiber.Ctx) error {
+	authorized := utils.AuthorizeRoute(c)
+	if !authorized {
+		return utils.UnauthorizedResponse(c)
+	}
+	auth := utils.GetAuthInfo(c)
+
+	var sites []models.Site
+
+	h.DB.Where("user_id = ?", auth.User.ID).Find(&sites)
+
+	return c.Status(fiber.StatusOK).JSON(sites)
+}
